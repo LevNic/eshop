28b44819d6dda183156ce72ccdd4dd3ad4f2cfd5
LevNic <klacc.f@gmail.com>
2021-05-19 16:57:01 +0500

    task 3

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
+// https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json – получить список товаров;
+// /getBasket.json – получить содержимое корзины;
+// /addToBasket.json – добавить товар в корзину;
+// /deleteFromBasket.json – удалить товар из корзины.
+
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
+function makeGETRequest(url, callback) {
+  var xhr;
+
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+    }
+  }
+
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
 class GoodsItem {
-  constructor(title, price) {
-    this.title = title;
+  constructor(product_name, price) {
+    this.product_name = product_name;
     this.price = price;
   }
 
+  // render() {
+  //   return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  // }
   render() {
-    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
   }
 }
 
@@ -14,19 +44,35 @@ class GoodsList {
     this.goods = [];
   }
 
-  fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-    ];
+  // fetchGoods() {
+  //   this.goods = [
+  //     { title: 'Shirt', price: 150 },
+  //     { title: 'Socks', price: 50 },
+  //     { title: 'Jacket', price: 350 },
+  //     { title: 'Shoes', price: 250 },
+  //   ];
+  // }
+
+  fetchGoods(cb) {
+    makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+      this.goods = JSON.parse(goods);
+      cb();
+    })
   }
+
+  // render() {
+  //   let listHtml = '';
+  //   this.goods.forEach(good => {
+  //     const goodItem = new GoodsItem(good.title, good.price);
+  //     listHtml += goodItem.render();
+  //   });
+  //   document.querySelector('.goods-list').innerHTML = listHtml;
+  // }
 
   render() {
     let listHtml = '';
     this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price);
+      const goodItem = new GoodsItem(good.product_name, good.price);
       listHtml += goodItem.render();
     });
     document.querySelector('.goods-list').innerHTML = listHtml;
@@ -38,10 +84,16 @@ class GoodsList {
   }
 }
 
-const list = new GoodsList;
-list.fetchGoods();
-list.getTotalSumm();
-list.render();
+// const list = new GoodsList;
+// list.fetchGoods();
+// list.getTotalSumm();
+// list.render();
+
+const list = new GoodsList();
+list.fetchGoods(() => {
+  list.render();
+});
+
 
 class Basket extends GoodsList {
   constructor(...args) {
